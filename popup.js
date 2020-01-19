1340d75d2987533de1569c52cfb864d520e3a1b9
abhisheksaxena1998 <abhi123923@gmail.com>
2020-01-19 23:44:56 +0530

    v1.3

--- popup.js
+++ popup.js
@@ -2,11 +2,14 @@
 
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
     var url = tabs[0].url;
+    theUrl="https://detecturl.herokuapp.com/api?query="+url;
     //console.log(url);
     if (url!="chrome://newtab/")
     {
-        alert (url);
+        alert (theUrl);
     }
+    
+
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         chrome.runtime.reload();
     });
