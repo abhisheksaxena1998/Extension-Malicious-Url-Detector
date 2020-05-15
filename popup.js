35a993c9cd7fae327fade7b8cdec3c191a0657e5
abhisheksaxena1998 <abhi123923@gmail.com>
2020-05-15 18:05:10 +0530

    updated extension

--- popup.js
+++ popup.js
@@ -2,61 +2,42 @@
 
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    theUrl="https://detecturl.herokuapp.com/api?query="+url;
-    theUrl2="https://malicious-url-detectorv5.herokuapp.com/result?url="+url;
+    theUrl="https://mudv7.eu-gb.cf.appdomain.cloud/api?query="+url;
+    theUrl2="https://mudv7.eu-gb.cf.appdomain.cloud/result?url="+url;
     //console.log(url);
+    //alert ("Hello")
+  
     if (url!="chrome://newtab/")
     {
-        //alert (theUrl);
-        /*var request = new XMLHttpRequest()
-
-        request.open('GET', theUrl, true)
-        request.onload = function() {
-        // Begin accessing JSON data here
-        var data = JSON.parse(this.response)
-
-        if (request.status >= 200 && request.status < 400) {
-            data.forEach(movie => {
-            console.log(movie.malware)
-            //alert(movie.malware)
-            })
-        } else {
-            console.log('error')
-        }
-        }
-
-        request.send()
-        alert(request.responseText.malware)*/
-
         fetch(theUrl2)
+        //console.log("Script Started");
+        var obj;
+
         fetch(theUrl)
-        .then(response => response.json())
-        .then(data => {
-        //console.log(data.malware);
-        alert (url+"\n\n   <<-----MALWARE STATUS----->>   "+data.malware)// Prints result from `response.json()` in getRequest
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                obj = myJson;
+                //alert(obj.malware)
+                if (obj.malware == true)
+                {
+                    alert("Malicious URL Detected !!");
+                    if (window.confirm("Do you want to leave this unsafe site? \n\nThis site is not safe!!")) { 
+                        window.open("https://www.google.com/", "You have been re-directed to a safe site!!");
+                      }
+                }
 
-        //console.log(type(data));
-        if (data.malware==='false')
-        {
-            //console.log("false")
-            alert("this site is safe")
-            document.write("<h1>This site is safe</h1>");
-        }
-        else
-        {
-            document.write("Malicious Url!!")
-        }
-
-
-        })
-        //.catch(error => console.error(error))
+            });
+        console.log(obj);
+        
+        
     }
     
 
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         chrome.runtime.reload();
     });
-    //chrome.runtime.reload();
 });
 
 
@@ -65,12 +46,3 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': t
 
 
 
-
-
-
-//chrome.tabs.onCreated.addListener(function(tab) {         
-//    chrome.runtime.reload();
-// });
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//    chrome.runtime.reload();
-//});
